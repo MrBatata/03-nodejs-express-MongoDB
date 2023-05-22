96037bcfe66e29aa21e0a71038976261d813957a
MrBatata <mn.martinez@live.com.ar>
2023-05-22 18:41:42 -0300

    latest

--- app.js
+++ app.js
@@ -6,27 +6,34 @@ const app = express();
 /** Listen for requests */
 app.listen(3000);
 
+/** Register view engine
+ * Default folder for ejs files is 'views'
+ * If some other name, need to set as commented below
+ */
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
+
 /** Routes
  * Express automatically sets server statusCode and setHeader Content-Type
  */
 app.get('/', (req, res) => {
-  // res.send('<p>home page</p>');
-  // Express looks for an absolute path... not relative
-  // res.sendFile('C:/Users/Matias/Documentos/mr-batata/back-end/03-nodejs-express-MongoDB/views/index.html');
-  res.sendFile('./views/index.html', { root: __dirname });
+  const blogs = [
+    { title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+    { title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur' },
+  ];
+  res.render('index', { title: 'Home', blogs: blogs });
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>about page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about', { title: 'About' });
 });
 
-/** Redirects */
-app.get('/about-us', (req, res) => {
-  res.redirect('/about');
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
 /** 404 page */
 app.use((req, res) => {
-  res.status(404).sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404', { title: '404' });
 });
